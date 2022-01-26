1b78d1e5c9d49ca1c54cf42c4f940c0f164cadd2
unknown <sernamartin46@gmail.com>
2022-01-26 11:39:29 -0600

    add rule cors

--- src/app.js
+++ src/app.js
@@ -2,9 +2,16 @@ import express from "express";
 import path from "path";
 import { engine } from "express-handlebars";
 import indexRoutes from "./routes/index.routes";
+import cors from "cors";
 import morgan from "morgan";
 
 const app = express();
+
+const corsOptions = {
+    origin:"*",
+    credentials: true,
+    optionSuccessStatus: 200,
+}
 
 app.set("views", path.join(__dirname + "/views"));
 
@@ -19,6 +26,7 @@ app.set("view engine", "hbs");
 //middlewares
 app.use(morgan("dev"));
 app.use(express.urlencoded({extended:false}));
+app.use(cors(corsOptions));
 //routes
 app.use(indexRoutes);
 //static files
